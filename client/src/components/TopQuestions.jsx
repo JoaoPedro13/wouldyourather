68d935dedb8e309477b4ad5da1b6c63020302427
JoaoPedro13 <jpc130@gmail.com>
2019-12-16 19:27:01 +0000

    maintenance

--- client/src/components/TopQuestions.jsx
+++ client/src/components/TopQuestions.jsx
@@ -1,29 +1,26 @@
-import React, { Component, Fragment } from 'react'
-import { getTopQuestions } from "./../services/contentServices"
-import { Link } from "react-router-dom"
+import React, { Component, Fragment } from "react";
+import { getTopQuestions } from "./../services/contentServices";
+import { Link } from "react-router-dom";
 
 export class TopQuestions extends Component {
   constructor(props) {
     super(props);
-    this.state = { topList: "" }
+    this.state = { topList: "" };
     this.getTop = this.getTop.bind(this);
   }
 
-
   async getTop() {
     try {
-
       const retrievedTopQuestions = await getTopQuestions();
       console.log(retrievedTopQuestions.data);
       this.setState({ topList: retrievedTopQuestions.data });
-
-    } catch (error) { console.log(error); }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   componentDidMount() {
     this.getTop();
-
-
   }
 
   render() {
@@ -31,11 +28,16 @@ export class TopQuestions extends Component {
       <Fragment>
         <h3>Most Popular Questions</h3>
         <ol>
-          {this.state.topList && this.state.topList.map(question => <Link to={"/post/" + question._id} > <li>{question.title}</li></Link>)}
+          {this.state.topList &&
+            this.state.topList.map(question => (
+              <Link to={"/post/" + question._id}>
+                <li>{question.title}</li>
+              </Link>
+            ))}
         </ol>
-      </Fragment >
-    )
+      </Fragment>
+    );
   }
 }
 
-export default TopQuestions
+export default TopQuestions;
