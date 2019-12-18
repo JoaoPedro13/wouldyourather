de4cce9307b7798a5ea40f7fb9df7154aa943a21
José Maria Pereira <zemaria@gmail.com>
2019-12-18 15:12:31 +0000

    meta tags on stats

--- client/src/views/QuestionStats.jsx
+++ client/src/views/QuestionStats.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom"
 import { getQuestion } from "./../services/contentServices"
 import Chart from "./../components/Chart"
 import Share from '../components/Share';
+import MetaTags from 'react-meta-tags';
+
+
 
 export class QuestionStats extends Component {
   constructor(props) {
@@ -37,38 +40,51 @@ export class QuestionStats extends Component {
   render() {
 
     return (
-      <div>
-        <h1>Stats!</h1>
-        {this.state.currentQuestion && <Fragment>
-          <p>Created by {this.state.currentQuestion.authorID.name} in {this.state.currentQuestion.createdAt}</p>
-          <p>Total answers: {this.state.currentQuestion.answers.length}</p>
-          <p>{this.state.currentQuestion.optionA}:{this.state.currentQuestion.answers.filter(answer =>
-            answer.option === "A"
+      <Fragment>
+        <MetaTags>
+
+          <meta id="description" name="description" content="world's best dilemmas" />
+          <meta id="ogtitle" property="og:title" content="would you rather" />
+          <meta id="ogdescription" property="og:description" content="world's best dilemmas" />
+          <meta id="ogimage" property="og:image"
+            content="https://res.cloudinary.com/db1i5vxr8/image/upload/v1576674314/nespera%20pics/ASD_j2cv0w.png" />
+          <meta id="ogtype" property="og:type" content="website" />
+
+        </MetaTags>
+
+        <div>
+          <h1>Stats!</h1>
+          {this.state.currentQuestion && <Fragment>
+            <p>Created by {this.state.currentQuestion.authorID.name} in {this.state.currentQuestion.createdAt}</p>
+            <p>Total answers: {this.state.currentQuestion.answers.length}</p>
+            <p>{this.state.currentQuestion.optionA}:{this.state.currentQuestion.answers.filter(answer =>
+              answer.option === "A"
 
 
-          ).length}</p>
-          <p>{this.state.currentQuestion.optionB}:{this.state.currentQuestion.answers.filter(answer =>
-            answer.option === "B"
+            ).length}</p>
+            <p>{this.state.currentQuestion.optionB}:{this.state.currentQuestion.answers.filter(answer =>
+              answer.option === "B"
 
 
-          ).length}</p>
+            ).length}</p>
 
 
 
-          <Chart amountForA={this.state.currentQuestion.answers.filter(answer =>
-            answer.option === "A"
+            <Chart amountForA={this.state.currentQuestion.answers.filter(answer =>
+              answer.option === "A"
 
 
-          ).length} amountForB={this.state.currentQuestion.answers.filter(answer =>
-            answer.option === "B"
+            ).length} amountForB={this.state.currentQuestion.answers.filter(answer =>
+              answer.option === "B"
 
 
-          ).length} />
-        </Fragment >}
-        <Share id={this.props.match.params.questionId} />
+            ).length} />
+          </Fragment >}
+          <Share id={this.props.match.params.questionId} />
 
-        <Link to="/post/random">Another Question! </Link>
-      </div>
+          <Link to="/post/random">Another Question! </Link>
+        </div>
+      </Fragment>
     )
   }
 }
