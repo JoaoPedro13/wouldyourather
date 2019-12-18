a9d835d7d0b64af6b405fcb8e0fe6695ebc86c96
José Maria Pereira <zemaria@gmail.com>
2019-12-18 13:13:20 +0000

    share links fixed

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -4,9 +4,9 @@ const Share = (props) => {
 
   return (
     <div>
-      <a href={"whatsapp://send?text=http://localhost.com/post/" + props.id} data-action="share/whatsapp/share">Share via Whatsapp</a>
-      <a href={"https://www.facebook.com/sharer/sharer.php?u=" + "http://localhost.com/post/" + props.id} data-action="share/whatsapp/share">Share via Facebook</a>
-      <a href="https://twitter.com/share?ref_src=www.google.com" className="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+      <a href={"whatsapp://send?text=" + process.env.REACT_APP_DOMAIN + "/post/" + props.id} data-action="share/whatsapp/share">Share via Whatsapp</a>
+      <a href={"https://www.facebook.com/sharer/sharer.php?u=" + process.env.REACT_APP_DOMAIN + "/post/" + props.id} data-action="share/whatsapp/share">Share via Facebook</a>
+      <a href={"https://twitter.com/share?ref_src=" + process.env.REACT_APP_DOMAIN + "/post/"} className="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
 
     </div>
   )
