85595427a5e2a86f22bebf347ac9d726297a3e8e
JoaoPedro13 <jpc130@gmail.com>
2019-12-19 13:18:26 +0000

    forms styles done

--- client/src/views/CreateQuestion.jsx
+++ client/src/views/CreateQuestion.jsx
@@ -31,47 +31,74 @@ export class CreateQuestion extends Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.formHandler}>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            onChange={this.handleChange}
-          />
-
-          <label htmlFor="optionA">optionA</label>
-          <input
-            type="text"
-            id="optionA"
-            name="optionA"
-            onChange={this.handleChange}
-          />
-
-          <label htmlFor="optionB">optionB</label>
-          <input
-            type="text"
-            id="optionB"
-            name="optionB"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="category">category</label>
-          <select
-            name="category"
-            onChange={this.handleChange}
-            value={this.state.category}
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            <option value="heart">Love</option>
-            <option value="football">Sports</option>
-            <option value="wrench">Work</option>
-            <option value="dolar">Money</option>
-          </select>
-
-          <button>Create a dilema</button>
+      <div className="container">
+        <form onSubmit={this.formHandler} className="createQuestion-form">
+          <div className="col-auto">
+            <label className="sr-only" htmlFor="inlineFormInput">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              onChange={this.handleChange}
+              className="form-control mb-2 mr-sm-2"
+              id="inlineFormInputName2"
+            />
+          </div>
+          <div className="col-auto">
+            <label className="sr-only" htmlFor="inlineFormInput">
+              optionA
+            </label>
+            <input
+              type="text"
+              id="optionA"
+              name="optionA"
+              onChange={this.handleChange}
+              className="form-control mb-2 mr-sm-2"
+              id="inlineFormInputName2"
+            />
+          </div>
+          <div className="col-auto">
+            <label className="sr-only" htmlFor="inlineFormInput">
+              optionB
+            </label>
+            <input
+              type="text"
+              id="optionB"
+              name="optionB"
+              onChange={this.handleChange}
+              className="form-control mb-2 mr-sm-2"
+              id="inlineFormInputName2"
+            />
+          </div>
+          <div className="col-auto">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <label className="input-group-text" for="inputGroupSelect01">
+                  category
+                </label>
+              </div>
+              <select
+                name="category"
+                onChange={this.handleChange}
+                value={this.state.category}
+                className="custom-select"
+                id="inputGroupSelect01"
+              >
+                <option value="" disabled>
+                  Select one...
+                </option>
+                <option value="heart">Love</option>
+                <option value="football">Sports</option>
+                <option value="wrench">Work</option>
+                <option value="dollar">Money</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-outline-dark">Create a dilema</button>
+          </div>
         </form>
       </div>
     );
